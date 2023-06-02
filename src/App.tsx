e996801412e42833a5685a6529589fda7e906ceb
Dzhusuev Adilet <adilet.djusuev@gmail.com>
2023-06-02 23:37:55 +0600

    добавил новый массив, в котором будут храниться выбранные ингридиенты, при нажатии на изображение срабатывает счетчик

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,76 @@ import cheeseImage from './assets/ingridients/cheese.png';
 import saladImage from './assets/ingridients/salad.png';
 import baconImage from './assets/ingridients/bacon.png';
 import { nanoid } from 'nanoid';
+import { count } from 'console';
 
 const App = () => {
   
-  const IgridientsArray: IIngridients[] = [
+  const IngridientsArray: IIngridients[] = [
     { id: nanoid(), name: 'Meat', price: 80, image: meatImage },
-    { id: nanoid(), name: 'cheese', price: 50, image: cheeseImage },
-    { id: nanoid(), name: 'salad', price: 10, image: saladImage },
-    { id: nanoid(), name: 'bacon', price: 60, image: baconImage },
+    { id: nanoid(), name: 'Cheese', price: 50, image: cheeseImage },
+    { id: nanoid(), name: 'Salad', price: 10, image: saladImage },
+    { id: nanoid(), name: 'Bacon', price: 60, image: baconImage },
   ];
 
   const [ingredients, setIngredients] = useState([
     {name: 'Meat', count: 0},
     {name: 'Cheese', count: 0},
-    {name: 'salad', count: 0},
-    {name: 'bacon', count: 0},
+    {name: 'Salad', count: 0},
+    {name: 'Bacon', count: 0},
   ]);
 
-  const ingButtons = IgridientsArray.map((item) => (
-    <button key={item.id}>{item.name}</button>
+  const handleIngredientClick = (index: number) => {
+    const updatedIngredients = [...ingredients];
+    updatedIngredients[index].count++;
+    setIngredients(updatedIngredients);
+  };
+
+  const ingredientComponents = IngridientsArray.map((item, index) => (
+    <div>
+      <button className='ingButtons' key={item.id} onClick={() => handleIngredientClick(index)}>
+        <img className='img' src={item.image} />
+        {item.name + " "}
+      </button>
+      <span className='count'>({"x" + ingredients[index].count})</span>
+      <button className='deleteBtn'>X</button>
+    </div>
   ));
+
+  const onDelete = (id: string) => {
+    setIngredients(prevState => prevState.filter(count => count.count --- 1))
+  };
+
+   const addBurgerIngredients = [];
+    for (let i = 0; i < ingredients.length; i++) {
+      for (let j = 0; j < ingredients[i].count; j++) {
+        addBurgerIngredients.push(
+          <div key={`${ingredients[i].name}-${j}`} className={ingredients[i].name}></div>
+        );
+      }
+    }
 
 
   return (
-    <div className="container">
-      <div className='rightBlock'>
-        <div>{ingButtons}</div>
-      </div>
-      <div className='leftBlock'> 
-        <div className="Burger">
-          <div className="BreadTop">
-            <div className="Seeds1"></div>
-            <div className="Seeds2"></div>
+    <div className='App'>
+      <div className="container">
+          <div className='leftBlock'>
+            <div>
+            {ingredientComponents}
+
+            </div>
           </div>
-          <div className="Salad"></div>
-          <div className="Cheese"></div>
-          <div className="Meat"></div>
-          <div className="BreadBottom"></div>
-        </div>
+        <div className='rightBlock'> 
+            
+          <div className="Burger">
+            <div className="BreadTop">
+              <div className="Seeds1"></div>
+              <div className="Seeds2"></div>
+            </div>
+            {addBurgerIngredients}
+            <div className="BreadBottom"></div>
+          </div>
+          <strong>Price: 30</strong>
+        </div>  
       </div>
     </div>
   );
